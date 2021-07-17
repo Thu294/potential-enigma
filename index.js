644aa7081cca26035fd767b4ec9c9969b9fd05e8
Tori Dang <mthud2904@gmail.com>
2021-07-16 20:16:11 -0700

    my third commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const { title } = require("process");
 
 
 // TODO: Create an array of questions for user input
+//array ask question the user
 const questions = [
     {
         type: `input`,
@@ -20,7 +21,71 @@ const questions = [
             }
         }
     },
-],
+
+//Description of project
+
+{
+    type: `input`,
+    name: `description`,
+    message: `Provide a description of the project (this is require)`,
+    validate: descriptionInput => {
+        if (descriptionInput) {
+            return true;
+        } else { 
+            console.log(`you need to provide a project description!`);
+            return false;
+
+        }
+    }
+},
+
+// Installation Instruction
+{
+    type: `input`,
+    name: `installation`,
+    message: ` How do you install your project/ (this is require)`,
+    validate: installationInput => {
+        if (installationInput) {
+            return true;
+        
+        } else {
+            console.log(`you need to provide installation info to continue`);
+            return false;
+        }
+    }
+},
+// Usage Infotmation
+{
+    type: `input`,
+    name: `usage`,
+    message: `how do you use this project? (this is require)`,
+    validate: usageInput => {
+        if (usageInput) {
+            return true;
+        } else {
+            console.log(`you need to provide information on how to use project!`);
+            return false;
+        }
+    }
+},
+
+// Contribution Guidlines
+{
+    type: `input`,
+    name: `contribution`,
+    message: `how should people contribute to this project?(this is require)`,
+    validate: contributionInput => {
+        if (contributionInput) {
+            return true;
+        } else {
+            console.log(`you need to provide information on how to contribute to the project!`);
+            return false;
+        }
+    }
+},
+
+
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
